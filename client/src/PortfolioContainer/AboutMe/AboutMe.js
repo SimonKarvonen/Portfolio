06479dbee47d6f2169607da4f89f0fa27c8a094b
Simon Karvonen <simonoskarkarvonen@gmail.com>
2021-12-07 09:45:38 +0100

    Added Testimonial and fixed some minor errors

--- client/src/PortfolioContainer/AboutMe/AboutMe.js
+++ client/src/PortfolioContainer/AboutMe/AboutMe.js
@@ -6,8 +6,7 @@ import "./AboutMe.css";
 
 export default function AboutMe(props) {
   let fadeInScreenHandler = (screen) => {
-    if (screen.fadeScreen !== props.id) return;
-
+    if (screen.fadeInScreen !== props.id) return;
     Animations.animations.fadeInScreen(props.id);
   };
 
@@ -40,7 +39,10 @@ export default function AboutMe(props) {
   };
 
   return (
-    <div className="about-me-container screen-container" id={props.id || ""}>
+    <div
+      className="about-me-container screen-container fade-in"
+      id={props.id || ""}
+    >
       <div className="about-me-parent">
         <ScreenHeading title={"About Me"} subHeading={"Why Choose Me?"} />
         <div className="about-me-card">
@@ -56,7 +58,13 @@ export default function AboutMe(props) {
               {renderHighlight()}
             </div>
             <div className="about-me-options">
-              <button className="btn primary-btn"> Hire Me </button>
+              <button
+                className="btn primary-btn"
+                onClick={() => ScrollService.scrollHandler.scrollToHireMe()}
+              >
+                {" "}
+                Hire Me{" "}
+              </button>
               <a href="Resume.pdf" download="Resume Simon Karvonen.pdf">
                 <button className="btn highlighted-btn">Get Resume</button>
               </a>
